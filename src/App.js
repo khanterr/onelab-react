09856bc76346b653d96ee08467ec1dd70636cf87
khanterr <nurgaliyev.adilkhan@gmail.com>
2024-02-28 10:36:30 +0600

    lesson1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import Counter from "./Counter";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count => count + 1)
+  }
+
+  useEffect(() => {
+    console.log("count updated:", count)
+  }, [count])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter count={count} handleClick={handleClick}/>
     </div>
   );
 }
